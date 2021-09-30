a0fd74ecbd7a044e9efaf62d0bd4bcac4b074b6a
Kevin Joshi <kevinjoshi46b@gmail.com>
2021-09-30 18:10:30 +0530

    Minor Ui updates + gymForm temp push

--- php/gymForm.php
+++ php/gymForm.php
@@ -12,20 +12,86 @@
 </head>
 
 <body>
+    <?php
+    if (isset($_POST["save"])) {
+        $error_email = "";
+        $error_phone = "";
+        $error_workout = "";
+        $error_gender = "";
+        $error_feeling = "";
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $workout = $_POST["workout"];
+        $gender = $_POST["gender"];
+        $feeling = $_POST["feeling"];
+        $error = false;
+        if (!preg_match("^[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+$", $email)) {
+            $error_email = "Invalid Email!";
+            $error = true;
+        }
+        if (!preg_match("^\d{10}$", $phone)) {
+            $error_phone = "Invalid phone number!";
+            $error = true;
+        }
+        if ($workout == ""){
+            $error_workout = "Select an input";
+            $error = true;
+        }
+        if ($gender == ""){
+            $error_gender = "Select an input";
+            $error = true;
+        }
+        if (!$error){
+            header("Location: php/index.php");
+        }
+    }
+    ?>
     <div class="top-section">
         <a href="#" class="back"><i class="fas fa-arrow-left fa-lg" style="color:white"></i></a>
         <h2 class="header">Today's Workout</h2>
     </div>
     <div class="content">
-        <form method="POST">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="text" name="email" id="email" class="form-control">
+            </div>
+            <p class="small"><?php echo $error_email ?></p>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone number:</label>
+                <input type="number" name="phone" id="phone" class="form-control">
+            </div>
+            <p class="small"><?php echo $error_phone ?></p>
             <div class="mb-3">
                 <label for="workout" class="form-label">Workout:</label>
                 <select name="workout" id="workout" class="form-select">
-                    <option value="Select" default>Select</option>
+                    <option value="" default>Select</option>
                     <option value="Hammer Curl">Hammer Curl</option>
                 </select>
             </div>
+            <p class="small"><?php echo $error_workout ?></p>
             <div class="mb-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gender" id="male">
+                    <label class="form-check-label" for="male">
+                        Male
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gender" id="female">
+                    <label class="form-check-label" for="female">
+                        Female
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gender" id="other">
+                    <label class="form-check-label" for="other">
+                        Other
+                    </label>
+                </div>
+            </div>
+            <p class="small"><?php echo $error_gender ?></p>
+            <!-- <div class="mb-3">
                 <label for="sets" class="form-label">Number of sets:</label>
                 <input type="number" name="sets" id="sets" class="form-control">
             </div>
@@ -36,13 +102,14 @@
             <div class="mb-3">
                 <label for="time" class="form-label">Total time taken to complete all sets: (in minutes)</label>
                 <input type="number" name="time" id="time" class="form-control">
-            </div>
+            </div> -->
             <div class="mb-3">
                 <label for="energy" class="form-label">How Energetic do you feel? (0 to 100)</label>
-                <input type="range" class="form-range" id="energy" min="0" max="100" value="20">
+                <input name="feeling" type="range" class="form-range" id="energy" min="0" max="100" value="20">
             </div>
+            <p class="small"><?php echo $error_feeling ?></p>
             <div class="submission">
-                <button type="submit" class="submit">Save</button>
+                <button type="submit" class="submit" name="save">Save</button>
             </div>
         </form>
     </div>
--- php/yogaForm.php
+++ php/yogaForm.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Athleap: Yoga Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/yogaStyle.css">
     <link href="../fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -28,18 +28,15 @@
         } else if (preg_match("/^[a-zA-Z]*$/", $_POST["duration"])) {
             // echo "<span style='color:red;'>Duration cannot be letter</span><br>";
             $durationErr = "Duration cannot be letter";
-        }
-        else if((int)($_POST["duration"])<=0){
+        } else if ((int)($_POST["duration"]) <= 0) {
             $durationErr = "Duration should be greater than 0";
-        }
-        else {
+        } else {
             $duration = $_POST["duration"];
         }
 
-        if(empty($_POST["yogaAsan"])) {
+        if (empty($_POST["yogaAsan"])) {
             $yogaErr = "Please select an Asana";
-        }
-        else{
+        } else {
             $yoga = $_POST["yogaAsan"];
         }
 
@@ -48,7 +45,7 @@
     ?>
 
     <div class="top-section">
-        <a href="#" class="back"><i class="fas fa-arrow-left fa-lg" style="color:white"></i></a>
+        <a href="#" class="back"><i class="fas fa-bars fa-lg" style="color:white"></i></a>
         <h2 class="header">Today's Workout</h2>
     </div>
 
@@ -110,9 +107,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="text-center row align-center my-5">
+                <div class="row align-center my-5">
                     <div class="col"></div>
-                    <button class="btn btn-md col-2 submit-btn" name="save"> Submit </button>
+                    <div class="col">
+                        <button class="btn btn-md submit-btn" name="save">Submit</button>
+                    </div>
                     <div class="col"></div>
                 </div>
                 <div class="text-center row align-center my-5">
@@ -120,9 +119,8 @@
                     <div class="col">
                         <?php
                         if ($duration && $yoga && $rating) {
-                            echo '<span> Duration: ' . $duration ." mins<br>Yoga: ". $yoga ."<br>Rating: ". $rating .'<br></span>';
-                        }
-                        else{
+                            echo '<span> Duration: ' . $duration . " mins<br>Yoga: " . $yoga . "<br>Rating: " . $rating . '<br></span>';
+                        } else {
                             echo '<span> Invalid Inputs Entered </span>';
                         }
                         ?>
