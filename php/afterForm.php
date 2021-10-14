315d6b0466234f795fc2fb7b9522737846bf3370
Kaushal <kaushal.binjola11@gmail.com>
2021-10-14 15:13:28 +0530

    started afterForm page animation

---
+++ php/afterForm.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Athleap: Result</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet">
+    <link href="../bootstrap-5.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/afterForm.css" rel="stylesheet">
+    <link href="../css/common.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="top-section">
+        <a href="#" class="back"><i class="fas fa-bars fa-lg" style="color:white"></i></a>
+        <h2 class="header">Result</h2>
+    </div>
+
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-6 parent">
+            <div class="calorie-result">
+                <h1>Calories Burnt</h1>
+                <br>
+                <h4 style="margin-top: -30px;">100</h4>
+            </div>
+            <div class="star2">
+            </div>
+            <div class="star">
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+
+</body>
+
+</html>
